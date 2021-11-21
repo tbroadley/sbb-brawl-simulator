6e818cd4bfe57282a25a97003c599cb86b716c11
Thomas Broadley <thomas@faire.com>
2021-11-21 18:43:20 -0500

    Remove hero base health

--- src/main/kotlin/com/sbb/heroes/Hero.kt
+++ src/main/kotlin/com/sbb/heroes/Hero.kt
@@ -2,14 +2,11 @@ package com.sbb.heroes
 
 enum class Hero(
     val humanReadableName: String,
-    val baseHealth: Long,
 ) {
     APOCALYPSE(
         humanReadableName = "Apocalypse",
-        baseHealth = 50,
     ),
     SIR_GALAHAD(
         humanReadableName = "Sir Galahad",
-        baseHealth = 40,
     ),
 }
